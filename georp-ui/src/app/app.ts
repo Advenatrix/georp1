ac3712184d7328e0e43c78cbc43a56fb285e1ab7
Advenatrix <enders.sky@gmail.com>
2025-07-22 13:25:26 +0000

    css test

--- georp-ui/src/app/app.ts
+++ georp-ui/src/app/app.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+
+interface NavLink {
+  label: string;
+  path: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss'],
+  standalone: true,
+  imports: [RouterModule],
 })
-export class App {
-  protected title = 'georp-ui';
+export class AppComponent {
+  title = 'Georp UI';
+  navLinks: NavLink[] = [
+    { label: 'Home',  path: '/' },
+    { label: 'Docs',  path: '/docs' },
+    { label: 'About', path: '/about' },
+  ];
+
+  trackByLabel(_: number, link: NavLink) {
+    return link.label;
+  }
 }
